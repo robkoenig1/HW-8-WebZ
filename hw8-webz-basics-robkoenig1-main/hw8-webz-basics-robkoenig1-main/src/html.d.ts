967e0f24b181ca06e1817f4da43eddee9033ae27
robkoenig1 <145148658+robkoenig1@users.noreply.github.com>
2024-10-27 01:01:42 -0400

    Delete hw8-webz-basics-robkoenig1-main directory

--- hw8-webz-basics-robkoenig1-main/hw8-webz-basics-robkoenig1-main/src/html.d.ts
+++
@@ -1,9 +0,0 @@
-// typings.d.ts
-declare module "*.html" {
-    const content: string;
-    export default content;
-}
-declare module "*.css" {
-    const content: string;
-    export default content;
-}
